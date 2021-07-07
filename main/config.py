ef57094b8322754c454d8267e8283da152d9f4b3
k-fujishiro <fujisiro@ae.auone-net.jp>
2021-07-07 14:01:19 +0900

    solution to sqlalchemy 1.4.x.

--- main/config.py
+++ main/config.py
@@ -1,5 +1,10 @@
 import os
+import re
 
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or "sqlite:///test.db"
+uri = os.getenv("DATABASE_URL")
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+
+SQLALCHEMY_DATABASE_URI = os.environ.get(uri) or "sqlite:///test.db"
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SECRET_KEY = "grzZfUYtGD5R9"
